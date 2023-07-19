6d11194966c902bb56726c07c973426bba18ef33
Kevin Baxter <kbaxter@logofit.com>
2023-07-19 10:48:34 -0400

    removed more fields

--- SOButtonSublistUEScript.js
+++ SOButtonSublistUEScript.js
@@ -26,8 +26,14 @@ define(['N/currentRecord', 'N/record', 'N/recordContext', 'N/redirect', 'N/rende
 
             try{
                 const userObj = runtime.getCurrentUser();
-                const {record} = scriptContext;
-                log.debug("This is a user", userObj)
+                const record = scriptContext.newRecord
+                var rallyHouseVPN = record.getSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'quantityavailable',
+                    line: 1
+                })
+
+
 
                 if(userObj.role === 1047){
                     var schoolName = record.getSublistValue({
@@ -36,17 +42,52 @@ define(['N/currentRecord', 'N/record', 'N/recordContext', 'N/redirect', 'N/rende
                         line: 1
                     });
 
-                    if(!schoolName){
+
+
+
+
+                    if(rallyHouseVPN){
                         hideColumnField(scriptContext.form , 'item', 'custcol_lf_school_name')
                     }
 
                     hideColumnField(scriptContext.form , 'item', 'custcol_lf_follett_retail')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_design')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_price_level')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_image_url')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_bn_sku')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_flc_sku')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_flc_retail')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_item_license_approved')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_follett_upc')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_follett_can_retail')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_fol_art')
+                    hideColumnField(scriptContext.form , 'item', 'custcolcustcol_lf_fol_detail_art')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_rally_house_sku')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_rally_house_retail')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_mden_sku')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_mden_retail')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_ind_upc')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_sku')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_ind_retail')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_fanatics_sku')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_fanatics_venue_ret')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_royalty_exempt_item')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_has_note_check')
+                    hideColumnField(scriptContext.form , 'item', 'quantitycommitted')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_rally_house_upc')
+                    hideColumnField(scriptContext.form , 'item', 'isclosed')
+                    hideColumnField(scriptContext.form , 'item', 'options')
+                    hideColumnField(scriptContext.form , 'item', 'orderpriority')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_rally_house_vpn')
+                    hideColumnField(scriptContext.form , 'item', 'custcol_lf_fanatics_retail')
+
                 }
 
                 if(userObj.department === 4){
                     hideColumnField(scriptContext.form , 'item', 'class')
                     hideColumnField(scriptContext.form, 'item', 'department')
                 }
+
             }catch (e) {
                 log.error({
                     title: 'BeforLoad Error',
